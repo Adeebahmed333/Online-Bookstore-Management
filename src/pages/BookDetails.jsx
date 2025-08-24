15287fdfa190ecc59b3b3a587587db8c1f4645bb
Adeebahmed333 <adeebahmed3337@gmail.com>
2025-08-24 11:07:01 +0530

    Completed the book details page and done the necessary styling and conditional styling as well.

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
-
+import axios from "axios";
+import { useState, useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom";
 const BookDetails = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/books/${id}`);
+        setData(response.data);
+        console.log("book response", response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [id]);
+
   return (
-    <div>BookDetails</div>
+    <div className="card bg-black text-white d-flex m-auto mt-4 text-center" style={{ width: "300px", height: "300px", borderRadius: "5px" }}>
+      <div className="card-header">
+        <div className="h3">{data?.title}</div>
+      </div>
+      <div className="card-body">
+        <div className="card-text">{data?.title}</div>
+        <div className="card-text">
+          Author: {data.author}
+        </div>
+        <div className="card-text">
+          Genre: {data.genre}
+        </div>
+        <div className="card-text">
+          Price: {data.price}
+        </div>
+        <div className="card-text" style={{ color: (data.stock > 0) ? "green" : "red" }}>
+          Stock: {data.stock}
+        </div>
+      </div>
+      <div className="card-footer d-flex" style={{ justifyContent: "space-between" }}>
+        <button className="btn btn-danger" >Delete Book</button>
+        <button className="btn btn-warning" onClick={() => { navigate(`/edit-book/${id}`) }}>Edit Book</button>
+      </div>
+    </div>
   )
 }
 
