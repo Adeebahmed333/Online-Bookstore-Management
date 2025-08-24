451fc121beec7d5c0724b475aae4d99de2cbf242
Adeebahmed333 <adeebahmed3337@gmail.com>
2025-08-24 11:29:10 +0530

    Made table component and done the necessary styling as well

--- src/pages/BooksTable.jsx
+++ src/pages/BooksTable.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const BooksTable = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/books");
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>BooksTable</div>
+    <div className="container">
+      {
+        (data.length > 0) ? (
+          <table className="table table-striped table-bordered table-hover bg-dark mt-4">
+            <thead className="table-dark">
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Genre</th>
+                <th>Price</th>
+                <th>Stock</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              {
+
+                data.map((item) => {
+                  return (
+                    <tr>
+                      <td>
+                        {
+                          item.id
+                        }
+                      </td>
+                      <td>
+                        {
+                          item.title
+                        }
+                      </td>
+                      <td>
+                        {
+                          item.author
+                        }
+                      </td>
+                      <td>
+                        {
+                          item.genre
+                        }
+                      </td>
+                      <td>
+                        {
+                          item.price
+                        }
+                      </td>
+                      <td>
+                        {
+                          item.stock
+                        }
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        ) : (
+          <h1 className="text-danger">No Books To Show!!!</h1>
+        )
+      }
+    </div>
   )
 }
 
